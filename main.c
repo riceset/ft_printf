550cd9061ad9bdd9b052adeea6ae842534374968
komeno <tkomeno@student.42tokyo.jp>
2022-04-30 02:44:24 -0300

    ✨ gitignore

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 01:17:18 by tkomeno           #+#    #+#             */
-/*   Updated: 2022/04/30 02:29:48 by tkomeno          ###   ########.fr       */
+/*   Updated: 2022/04/30 02:41:51 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int	main(void)
 {
-	ft_printf("%x", -10);
+	// TEST(2, print(" %x ", -1));
+	ft_printf(" %x ", -1);
+	// printf(" %x ", -1);
 	// printf("ft:\t%d\n",
 	// 		ft_printf("%%%%%%x %x %X %d %s %c\n", 10, 10, 17, "SEVENTEEN", 's'));
 	// printf("or:\t%d\n",
